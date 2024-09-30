01f19cbfe401d7dd6e4759e76a35f3f248862fe1
cedrix <cedrixdacanay27@gmail.com>
2024-09-30 13:43:59 +0800

    Finish working with index and show page

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,23 @@
+class SubjectsController < ApplicationController
+
+  layout false
+
+  def index
+    @subjects = Subject.sorted
+    render(:template => 'subjects/index')
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    render(:template => 'subjects/index')
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+  resources :subjects
+
   # get 'demo/index'
 
   get 'demo/index'
