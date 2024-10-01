822695714736f308690b8838306c477990aaf2a4
cedrix <cedrixdacanay27@gmail.com>
2024-10-01 08:20:46 +0800

    Finished Pages CRUD

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,30 +16,40 @@ class PagesController < ApplicationController
 
   def create
     @page = Page.new(page_params)
-    
-   if @page.save
-      flash[:notice] = "Page created successfully"
-      redirect_to(:action => 'index')
-  else
-    render('new')
-  end
-
+    if @page.save
+        flash[:notice] = "Page created successfully"
+        redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Page updated successfully"
+      redirect_to(:action => 'show', :id => @page.id)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @page = Page.find(params[:id])
   end
 
   def destroy
+    page = Page.find(params[:id]).destroy
+    flash[:notice] = "#{page} Deleted Successfully"
+    redirect_to(:action => 'index')
   end
 
   private 
     def page_params
-      params.require(:page).permit(:name, :position, :permalink, :visible)
+      params.require(:page).permit(:subject_id, :name,:permalink, :position, :visible)
     end
 end
