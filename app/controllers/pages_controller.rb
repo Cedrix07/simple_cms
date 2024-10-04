84dbb49b3749b9948ea99e514b7a04d927069e75
cedrix <cedrixdacanay27@gmail.com>
2024-10-04 11:53:18 +0800

    nested page and subject

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,10 @@ class PagesController < ApplicationController
   layout "admin"
   # protect pages for unauthenticate users 
   before_action :confirmed_logged_in
-
+  before_action :find_subject
   def index
-    @pages = Page.sorted
+    # @pages = Page.where(:subject_id => @subject.id).sorted
+    @pages = @subject.pages.sorted 
   end
  
   def show
@@ -13,7 +14,7 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
+    @page = Page.new({:subject_id => @subject.id, :name => "Default"})
     @subjects = Subject.order('position ASC')
     @page_count = Page.count + 1
   end
@@ -22,7 +23,7 @@ class PagesController < ApplicationController
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully!"
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :subject_id => @subject.id)
     else
       @subjects = Subject.order('position ASC')
       @page_count = Page.count + 1
@@ -33,7 +34,7 @@ class PagesController < ApplicationController
 
   def edit
     @page = Page.find(params[:id])
-    @subjects = Subject.order_by('position ASC')
+    @subjects = Subject.order('position ASC')
     @page_count = Page.count
   end
 
@@ -41,9 +42,9 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successfully"
-      redirect_to(:action => 'show', :id => @page.id)
+      redirect_to(:action => 'show', :id => @page.id, :subject_id => @subject.id)
     else
-      @subjects = Subject.order_by('position ASC')
+      @subjects = Subject.order('position ASC')
       @page_count = Page.count
       render('edit')
     end
@@ -56,11 +57,18 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "#{page.name} Deleted Successfully"
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :subject_id => @subject.id)
   end
 
   private 
     def page_params
       params.require(:page).permit(:subject_id, :name,:permalink, :position, :visible)
+    end
+
+    def find_subject 
+      if params[:subject_id] 
+        @subject = Subject.find(params[:subject_id])
+
+      end
     end
 end
